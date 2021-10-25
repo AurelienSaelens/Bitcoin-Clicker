f3f8fd70d73f8db2602083c463cf82aaa8c06116
Aurélien Saelens <aureliensaelens0@gmail.com>
2021-10-25 20:59:52 +0200

    BitcoinClicker

--- bitcoinClicker/script.js
+++ bitcoinClicker/script.js
@@ -2,8 +2,13 @@ let score = 0;
 let addPower = 0;
 
 function addToscore(n, addPower) {
-    if (addPower == 0) {
         score = parseInt(score) + 1;
         document.getElementById('score').innerHTML = score;
-    }
 }
+
+function reset () {
+        score = 0;
+        document.getElementById('score').innerHTML = 0;
+}
+
+  
